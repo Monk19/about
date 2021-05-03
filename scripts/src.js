cdbd20cf066dcb59051e7d89a7da1232519abbb5
Monk19 <ravigadu.19@gmail.com>
2021-05-04 01:26:13 +0530

    safe

--- scripts/src.js
+++ scripts/src.js
@@ -1,13 +1,43 @@
-const pElement = document.getElementById("typer");
-const toRenderContent = `Hello I am Ravi and i am web Developer
-Intrested in UI/UX and all round web development`;
-let i = 0;
-let timeFunction = setInterval(dynamicType, 100);
-function dynamicType() {
-  if (i >= toRenderContent.length) {
-    clearInterval(timeFunction);
-  } else {
-    pElement.innerHTML += toRenderContent[i];
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
   }
-  i++;
 }
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
